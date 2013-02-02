12b56fca3699dccb2b2f53bdf4d04e67605998bb
Matt Kirwan <matthewkirwan@gmail.com>
2013-02-02 20:24:37 +0000

    Closed up some whitespace density within the method args

--- src/MattKirwan/TempToken/TempToken.php
+++ src/MattKirwan/TempToken/TempToken.php
@@ -13,32 +13,32 @@ class TempToken
 
 	private function __clone(){}
 
-	public function set_unique_salt( $salt )
+	public function set_unique_salt($salt)
 	{
 		$this->unique_salt = $salt;
 	}
 
-	public function set_token_lifetime_secs( $secs )
+	public function set_token_lifetime_secs($secs)
 	{
 		$this->token_lifetime_secs = $secs;
 	}
 
 	public function generate_token()
 	{
-		if( null !== $this->unique_salt )
+		if(null !== $this->unique_salt)
 		{
 			$this->unique_salt = rand();
 		}
 
 		$this->generate_token_expiration();
 
-		$this->token = md5( uniqid( $this->unique_salt, true ) );
+		$this->token = md5(uniqid($this->unique_salt, true));
 	}
 
 	private function generate_token_expiration()
 	{
 		$date = new \DateTime();
-		$date->add( new \DateInterval("PT{$this->token_lifetime_secs}S") );
+		$date->add(new \DateInterval("PT{$this->token_lifetime_secs}S"));
 
 		$this->token_expires_on = $date->format('Y-m-d H:i:s');		
 	}
