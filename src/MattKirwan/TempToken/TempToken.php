2493e9e35906095b501ad63222a16d8385b19690
Matt Kirwan <matthewkirwan@gmail.com>
2013-02-02 20:30:43 +0000

    Added a setter method allowing for date format override of the token expiration DateTime object

--- src/MattKirwan/TempToken/TempToken.php
+++ src/MattKirwan/TempToken/TempToken.php
@@ -10,6 +10,8 @@ class TempToken
 
 	private $unique_salt = null;	
 
+	private $date_format = 'Y-m-d H:i:s';
+
 	private function __clone(){}
 
 	public function set_unique_salt($salt)
@@ -20,6 +22,11 @@ class TempToken
 	public function set_token_lifetime_secs($secs)
 	{
 		$this->token_lifetime_secs = $secs;
+	}
+
+	public function set_date_format($date_format)
+	{
+		$this->date_format = $date_format;
 	}
 
 	public function generate_token()
@@ -39,7 +46,7 @@ class TempToken
 		$date = new \DateTime();
 		$date->add(new \DateInterval("PT{$this->token_lifetime_secs}S"));
 
-		$this->token_expires_on = $date->format('Y-m-d H:i:s');		
+		$this->token_expires_on = $date->format($this->date_format);		
 	}
 
 	public function get_token()
